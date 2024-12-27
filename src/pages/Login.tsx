714972ef99f71605d49ba8415c04b18120d8f407
Júlio Bem <juliocsbem@gmail.com>
2024-12-27 19:27:33 -0300

    feat: criar componente de botão comum

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import Button from '../components/OrdinaryButton'; // Caminho para o botão
 
 const Login: React.FC = () => {
 
   return (
     <div>
       <h1>Entra com sua conta Spotify clicando no botão abaixo</h1>
-      <button>Login</button>
+      <Button width='133px' height='42px'>Login</Button>
     </div>
   );
 };
