41f86836e2c50053c52e8dca2a6c41901f1e6749
Júlio Bem <juliocsbem@gmail.com>
2024-12-27 19:26:09 -0300

    feat: create login page

---
+++ src/pages/Login.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Login: React.FC = () => {
+
+  return (
+    <div>
+      <h1>Entra com sua conta Spotify clicando no botão abaixo</h1>
+      <button>Login</button>
+    </div>
+  );
+};
+
+export default Login;
