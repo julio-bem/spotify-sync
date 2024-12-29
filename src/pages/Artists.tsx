dd3bfd1032b9a1b179bab8d380b7e9e4906c58af
Júlio Bem <juliocsbem@gmail.com>
2024-12-28 21:25:42 -0300

    feat: fetch top artists from spotify api

--- src/pages/Artists.tsx
+++ src/pages/Artists.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import NavBar from '../components/NavBar';
 import PageHeading from '../components/PageHeading';
 import styled from 'styled-components';
@@ -15,7 +15,38 @@ const PageMainContainer = styled.section`
   gap: 16px;
 `;
 
+interface Artist {
+  id: string;
+  name: string;
+  images: { url: string }[];
+}
+
 const Artists: React.FC = () => {
+  const [topArtists, setTopArtists] = useState<Artist[]>([]);
+
+  useEffect(() => {
+    const fetchTopArtists = async () => {
+      const token = localStorage.getItem('accessToken');
+      const response = await fetch(
+        'https://api.spotify.com/v1/me/top/artists?limit=10&time_range=medium_term',
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+        setTopArtists(data.items);
+      } else {
+        console.error('Erro ao buscar os top artistas:', response.statusText);
+      }
+    };
+
+    fetchTopArtists();
+  }, []);
+
   return (
     <PageContainer>
       <NavBar activePage="artists" />
@@ -24,7 +55,13 @@ const Artists: React.FC = () => {
           title="Top Artistas"
           subtitle="Aqui você encontra seus artistas preferidos"
         />
-        <ArtistListItem profilePic="" name="" />
+        {topArtists.map((artist) => (
+          <ArtistListItem
+            key={artist.id}
+            profilePic={artist.images[0]?.url || ''}
+            name={artist.name}
+          />
+        ))}
       </PageMainContainer>
     </PageContainer>
   );
