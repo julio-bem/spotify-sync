a1764049311c9e1e21b3504a5a41bad878d9a890
Júlio Bem <juliocsbem@gmail.com>
2025-01-02 01:29:26 -0300

    feat: update e2e testing to new auth system

--- cypress/e2e/full_navigation_flow.cy.ts
+++ cypress/e2e/full_navigation_flow.cy.ts
@@ -1,18 +1,21 @@
 describe('Full Navigation Flow', () => {
+
     it('logs in, navigates through the app and creates a playlist', () => {
         cy.visit('/');
-        
-        cy.get('button').contains('Entrar').click();
-        
-// O redirecionamento do Spotify com um token de acesso foi simulado através de um mock que é obtido manualmente.
-// Tendo em vista, que não é possível automatizar o sistema de autenticação do Spotify para obter o token,
-// essa foi a forma que encontrei de simular o comportamento real com maior precisão sem mockar as chamadas a API.
 
-        const accessToken = 'BQAY7P107iyXLARg2CF-_5eXJ2i45rNTLFUKINUIySceKNtr6wiZwqq_5mKLWJ-M9pveQ8BYICHB7V4zkrIKWK05kRmRi04ktLVJ3uPhS1rfByel7y4pVxBgk53x5verhLxSLpfVL_m4cTYlpWqasDHCxKHsDDfCVpL3KYmBqBhhnt6Bj3vGs5CZ8bgNWDJvCItveCQJIbbHx4OAitAtytL_eeDG7rLb78yLRJ7jpl7wBRcZJ_W4lDzQ0JcHoaXDDmm_MsNrl9PCKl8MvpK9_yrYqUbcXcriRTW_3Fy5xiE'; // Type here the access token
+        // Substitua pelo token de acesso para simular o retorno da API do Spotify
+        const accessToken = '';
+        const tokenType = 'Bearer';
         const expiresIn = '3600';
+    
+        cy.window().then((window) => {
+          window.localStorage.setItem('accessToken', accessToken);
+          window.localStorage.setItem('tokenType', tokenType);
+          window.localStorage.setItem('expiresIn', expiresIn);
+        });
         
-        cy.visit(`/home#access_token=${accessToken}&token_type=Bearer&expires_in=${expiresIn}`);
-            
+        cy.visit(`/home`);
+
         cy.url().should('include', '/home');
         
         cy.get('p').contains('Artistas').click();
