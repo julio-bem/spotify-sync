de44433a032087f321ab3d1ad1a28c3ff70a8ab9
Júlio Bem <juliocsbem@gmail.com>
2025-01-23 15:42:11 -0300

    fix: unit testing

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,4 +1,20 @@
 import * as matchers from '@testing-library/jest-dom/matchers';
-import { expect } from 'vitest';
+import { expect, vi } from 'vitest';
+
+beforeAll(() => {
+    Object.defineProperty(window, 'matchMedia', {
+      writable: true,
+      value: (query: string) => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: vi.fn(),
+        removeListener: vi.fn(),
+        addEventListener: vi.fn(),
+        removeEventListener: vi.fn(),
+        dispatchEvent: vi.fn(),
+      }),
+    });
+  });
 
 expect.extend(matchers);
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,11 @@
-import { defineConfig } from 'vitest/config';
-import svgr from "vite-plugin-svgr";
+import { defineConfig } from 'vitest/config';import svgr from "vite-plugin-svgr";
 
-export default defineConfig({
-  plugins: [
-    svgr({svgrOptions: {
-      icon: true,
-    },}),
-  ],
+
+export default defineConfig({  plugins: [
+  svgr({svgrOptions: {
+    icon: true,
+  },}),
+],
   test: {
     environment: 'jsdom',
     globals: true,
